2884924f3480552d00a95a883fadbfd33b89f9c1
KaSaNaa <pasindudeshitha123@gmail.com>
2024-07-18 04:36:26 +0530

    refactor: Update ProjectItem component to include site name and handle card click

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,19 +1,45 @@
 import styled from "styled-components";
+import PropTypes from "prop-types";
 
-const ProjectItem = () => {
+// Utility function to extract the site name from a URL
+const extractSiteName = (url) => {
+  try {
+    const hostname = new URL(url).hostname;
+    // Split the hostname and return the second last part as the site name
+    const parts = hostname.split(".");
+    return parts.length > 2 ? parts[parts.length - 2] : parts[0];
+  } catch (error) {
+    console.error("Invalid URL", error);
+    return url; // Return the original URL if there's an error
+  }
+};
+
+const ProjectItem = ({ projectLink, projectName, topics, imageURL }) => {
+  ProjectItem.propTypes = {
+    projectLink: PropTypes.string.isRequired,
+    projectName: PropTypes.string.isRequired,
+    topics: PropTypes.string.isRequired,
+    imageURL: PropTypes.string.isRequired,
+  };
+
+  const handleCardClick = () => {
+    window.open(projectLink, "_blank");
+  };
+
+  const siteName = extractSiteName(projectLink);
   return (
-    <Card>
+    <Card onClick={handleCardClick}>
       <Details>
-        <p>project link</p>
-        <h1>Project Name</h1>
-        <span>related topics</span>
+        <ProjectLink>{siteName}</ProjectLink>
+        <h1>{projectName}</h1>
+        <span>{topics}</span>
       </Details>
       <Image>
         <img
-          src="https://via.placeholder.com/150"
+          src={imageURL}
           alt="project"
-          width={250}
-          height={250}
+          width={150}
+          height={150}
           style={{ borderRadius: "1rem" }}
         />
       </Image>
@@ -27,35 +53,42 @@ const Card = styled.div`
   display: flex;
   border-radius: 2rem;
   flex-direction: row;
-  width: 1311px;
-  height: 499px;
+  width: 500px;
   flex-shrink: 0;
+  height: 250px;
+  justify-content: space-evenly;
+  transition: box-shadow 0.6s ease-in-out;
+  margin-bottom: 10px;
 
   &:hover {
     box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.1);
+    transition: box-shadow 0.6s ease-in-out;
   }
+`;
+
+const ProjectLink = styled.h3`
+  font-size: 18px;
+  font-weight: 500;
+  letter-spacing: -0.5px;
+  color: #000; /* Adjust color as needed */
+  text-decoration: none;
 `;
 
 const Details = styled.div`
   display: flex;
-  margin-left: 5rem;
+  margin-left: 3rem;
   flex-direction: column;
   justify-content: center;
   width: 50%;
 
   h1 {
     font-weight: 500;
-    font-size: 48px;
+    font-size: 25px;
     letter-spacing: -0.96px;
-  }
-  p {
-    font-size: 24px;
-    font-weight: 500;
-    letter-spacing: -0.5px;
   }
   span {
     color: #999;
-    font-size: 24px;
+    font-size: 16px;
     font-weight: 500;
     letter-spacing: -0.48px;
   }
@@ -66,5 +99,6 @@ const Image = styled.div`
   width: 50%;
   justify-content: center;
   align-items: center;
-  margin-left: 40px;
+  justify-content: end;
+  margin-right: 3rem;
 `;
--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,15 +1,25 @@
 import styled from "styled-components";
 import ProjectItem from "../components/ProjectItem";
+import projectData from "../assets/data.json";
 
 const ProjectsSection = () => {
   return (
     <section id="projects">
-      <ProjectSection>
+      <Title>
         <p>
           <b>Projects</b>
         </p>
-        <ProjectItem />
-        <ProjectItem />
+      </Title>
+      <ProjectSection>
+        {projectData.map((project, index) => (
+          <ProjectItem
+            key={index}
+            projectLink={project.projectLink}
+            projectName={project.projectName}
+            topics={project.topics}
+            imageURL={project.imageURL}
+          />
+        ))}
       </ProjectSection>
     </section>
   );
@@ -17,15 +27,24 @@ const ProjectsSection = () => {
 
 export default ProjectsSection;
 
-const ProjectSection = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin: 3rem;
-  justify-content: center;
+const Title = styled.div`
+  width: 100%;
+  padding-left: 3rem;
+  align-self: start;
+  font-size: 36px;
+  font-weight: 500;
+  letter-spacing: -0.48px;
+`;
 
-  p {
-    font-size: 36px;
-    font-weight: 500;
-    letter-spacing: -0.48px;
+const ProjectSection = styled.div`
+  display: grid;
+  justify-items: center;
+  align-content: center;
+  grid-template-columns: repeat(2, 1fr); // Creates 2 columns
+  gap: 20px; // Adjust the gap between items as needed
+  padding: 20px; // Add some padding around the grid
+
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr; // On smaller screens, switch to 1 column
   }
 `;
