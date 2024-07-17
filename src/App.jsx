cb8026d20450f33a2d83c9548f6a265cb47da41e
KaSaNaa <pasindudeshitha123@gmail.com>
2024-07-18 00:14:19 +0530

    chore: Add ProjectsSection component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import LandingSection from "./sections/Landing";
 import "./App.css";
 import Navbar from "./sections/Navbar";
+import ProjectsSection from "./sections/Projects";
 
 function App() {
   return (
     <>
       <Navbar />
       <LandingSection />
+      <ProjectsSection />
     </>
   );
 }
