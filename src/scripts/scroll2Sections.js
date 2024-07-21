e476782cbd94e58af3a5b0a4a1b1ff5243979b47
KaSaNaa <pasindudeshitha123@gmail.com>
2024-07-21 18:23:16 +0530

    refactor: Add scrollToSection utility function

---
+++ src/scripts/scroll2Sections.js
@@ -0,0 +1,8 @@
+const scrollToSection = (sectionId) => {
+  const section = document.getElementById(sectionId);
+  if (section) {
+    section.scrollIntoView({ behavior: "smooth" });
+  }
+};
+
+export default scrollToSection;
