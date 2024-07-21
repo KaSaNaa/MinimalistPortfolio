04f7a7a8c23dfcfd418e44f23be903772637d509
KaSaNaa <pasindudeshitha123@gmail.com>
2024-07-21 18:23:27 +0530

    refactor: Update Footer component to dynamically render social icons from socials.json

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -8,96 +8,55 @@ import {
   FaSpotify,
   FaTiktok,
   FaReddit,
+  FaLink,
 } from "react-icons/fa";
 import { FaGoogle } from "react-icons/fa6";
+import Socials from "../data/socials.json";
 
 const Footer = () => {
-  const size = "30px";
+  const size = 30;
+  const getSocialIcon = (name) => {
+    switch (name) {
+      case "GitHub":
+        return <FaGithub size={size} />;
+      case "LinkedIn":
+        return <FaLinkedinIn size={size} />;
+      case "Twitter":
+        return <FaTwitter size={size} />;
+      case "Instagram":
+        return <FaInstagram size={size} />;
+      case "Blogger":
+        return <FaBlogger size={size} />;
+      case "Spotify":
+        return <FaSpotify size={size} />;
+      case "TikTok":
+        return <FaTiktok size={size} />;
+      case "Reddit":
+        return <FaReddit size={size} />;
+      case "Google":
+        return <FaGoogle size={size} />;
+      default:
+        return <FaLink size={size} />; // Default case if no icon matches
+    }
+  };
+
   return (
     <StyledFooter>
-      <Title><b>Find me on</b></Title>
-      <Container>
-        {/* Socials Section */}
-        <FooterSection id="socials">
+      <Title>
+        <b>Find me on</b>
+      </Title>
           <SocialLinks>
-            <a
-              href="https://twitter.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaTwitter to={"google.com"} size={size} />
-            </a>
-            <a
-              href="https://facebook.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaGithub size={size} />
-            </a>
-            <a
-              href="https://linkedin.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaLinkedinIn size={size} />
-            </a>
+            {Socials.map((social, index) => (
+              <a
+                key={index}
+                href={social.url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {getSocialIcon(social.name) }
+              </a>
+            ))}
           </SocialLinks>
-        </FooterSection>
-
-        {/* Contact Details Section */}
-        <FooterSection>
-          <SocialLinks>
-            <a
-              href="mailto:ranawakagepasindu@gmail.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaGoogle size={size} />
-            </a>
-            <a
-              href="https://pasinduranawakage.blogspot.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaBlogger size={size} />
-            </a>
-            <a
-              href="https://instagram.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaInstagram size={size} />
-            </a>
-          </SocialLinks>
-        </FooterSection>
-
-        {/* Additional Links Section */}
-        <FooterSection>
-          <SocialLinks>
-            <a
-              href="mailto:ranawakagepasindu@gmail.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaSpotify size={size} />
-            </a>
-            <a
-              href="mailto:ranawakagepasindu@gmail.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaTiktok size={size} />
-            </a>
-            <a
-              href="mailto:ranawakagepasindu@gmail.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaReddit size={size} />
-            </a>
-          </SocialLinks>
-        </FooterSection>
-      </Container>
     </StyledFooter>
   );
 };
@@ -111,34 +70,25 @@ const StyledFooter = styled.footer`
   box-shadow: 0px -2px 4px rgba(0, 0, 0, 0.1);
 `;
 
-const Container = styled.div`
-  display: flex;
-  max-width: 1200px;
-  margin: auto;
-  padding: 0 20px;
-`;
-
-const FooterSection = styled.div`
-  margin-bottom: 20px;
-  width: 100%;
-`;
-
 const SocialLinks = styled.div`
-  display: flex;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(40px, 1fr));
   flex-direction: column;
+  margin-bottom: 20px;
   height: 100%;
   justify-content: space-evenly;
   align-items: center;
 
   a {
+    justify-self: center;
     margin: 0 10px;
-    color: #333; /* Example icon color */
     transition: color 0.3s ease;
     padding: 1rem;
+    color: black;
     width: 10px;
 
     &:hover {
-      color: #007bff; /* Example hover color */
+      color: #007bff; // Blue color
     }
   }
 `;
